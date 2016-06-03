598b3d60e47a5d24f999ed0a8918ec78eab5bfec
Alessandro Valenti <alexiey@hotmail.it>
2016-06-03 12:36:29 +0200

    Server + client sistemato

--- src/main/java/it/uniroma2/fase5/Fase5Application.java
+++ src/main/java/it/uniroma2/fase5/Fase5Application.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class TrainingWorkshopApplication {
+public class Fase5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrainingWorkshopApplication.class, args);
+		SpringApplication.run(Fase5Application.class, args);
 	}
 }
--- src/main/java/it/uniroma2/fase5/ServletInitializer.java
+++ src/main/java/it/uniroma2/fase5/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TrainingWorkshopApplication.class);
+		return application.sources(Fase5Application.class);
 	}
 
 }
